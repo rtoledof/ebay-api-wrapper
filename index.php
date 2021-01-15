6f35c730c9bf65f75736ed74e9649e4637c8135c
Rolando Toledo Fernandez <rtoledofernandez@gmail.com>
2021-01-15 03:52:18 +0100

    add ebay wrapper service.

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php declare (strict_types = 1);
 
-require_once __DIR__ . '/../src/app.php';
+require_once __DIR__ . '/src/app.php';
 
 use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpFoundation\Request;
@@ -8,13 +8,13 @@ use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 
 $dotenv = new Dotenv();
-if (file_exists(__DIR__.'/../.env')) {
-	$dotenv->load(__DIR__.'/../.env');
+if (file_exists(__DIR__.'/.env')) {
+	$dotenv->load(__DIR__.'/.env');
 }
 
 $request = Request::createFromGlobals();
 $request->headers->add(['EBAY-API-KEY' => $_ENV['API_KEY']]);
-$routes  = include __DIR__ . '/../src/app.php';
+$routes  = include __DIR__ . '/src/app.php';
 
 $context = new RequestContext();
 $context->fromRequest($request);
